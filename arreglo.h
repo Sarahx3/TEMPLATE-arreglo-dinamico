aebd5e7ad8492b7ff28dc6cd549a80b13f7788cf
Aide Sarahi <aideflores5874@alumnos.udg.mx>
2020-10-29 14:04:05 -0600

    template arreglo dinamico con metodos eliminar

--- arreglo.h
+++ arreglo.h
@@ -11,7 +11,7 @@ private:
     T *arreglo;
     size_t tam;
     size_t cont;
-    const static size_t MAX = 5;
+    const static size_t MAX = 100;
 
     void expandir();
 
@@ -21,6 +21,11 @@ public:
 
     void insertar_inicio(const T& s);
     void insertar_final(const T& s);
+    void insertar(const T& s,size_t p);//ELEMENTO Y POSICION
+
+    void eliminar_inicio();
+    void eliminar_final();
+    void eliminar(size_t p);//posicion
     
     size_t size();
 
@@ -58,6 +63,7 @@ void Arreglo<T>::expandir(){
     arreglo= nuevo;
     tam = tam+MAX;
 }
+
 template <class T>
 void Arreglo<T>::insertar_final(const T& s){
     if (cont==tam){
@@ -79,4 +85,53 @@ void Arreglo<T>::insertar_inicio(const T& s){
     cont++;
 }
 
+template <class T>
+void Arreglo<T>::insertar(const T& s, size_t p){
+    if (p>=cont){
+        cout<<"Posicion NO valida"<< endl;
+        return;
+    }
+    if (cont==tam){
+        expandir();
+    }
+    for(size_t i = cont;i>p;i--){
+        arreglo[i]=arreglo[i-1];
+    }
+    arreglo[p] = s;
+    cont++;
+
+}
+
+template <class T>
+void Arreglo<T>::eliminar_final(){
+    if(cont==0){
+        cout<<"Arreglo vacio "<<endl;
+        return;
+    }
+    cont--;
+}
+
+template <class T>
+void Arreglo<T>::eliminar_inicio(){
+    if(cont==0){
+        cout<<"Arreglo vacio "<<endl;
+        return;
+    }
+    for (size_t i = 0; i < cont-1; i++){
+        arreglo[i]=arreglo[i+1];
+    }
+    cont--;
+}
+
+template <class T>
+void Arreglo<T>::eliminar(size_t p){
+    if(cont==0 || p > cont){
+        cout<<"Opcion no valida "<<endl;
+        return;
+    }
+    for (size_t i = p; i < cont-1; i++){
+        arreglo[i] = arreglo[i+1];
+    }
+    cont--;
+}
 #endif //ARREGLO_H
--- main.cpp
+++ main.cpp
@@ -6,12 +6,18 @@ int main()
 {
     Arreglo<float> arreglo;
 
-    for(int i=0;i<100.0;i++){
+    for(size_t i = 0; i < 10; i++){
         arreglo.insertar_final(i);
     }
-    for(size_t i=0.0;i<100.0;i++){
+    arreglo.insertar(100,2);
+    arreglo.eliminar_inicio();
+    arreglo.eliminar_final();
+    arreglo.eliminar(4);
+    for(size_t i = 0; i < arreglo.size(); i++){
         cout<<arreglo[i]<< " ";
     }
+    
     cout<<endl;
+
     return 0;
 }
