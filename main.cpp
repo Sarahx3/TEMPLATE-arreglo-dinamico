65f56fbcc0f4271eeb7822d4d2b211d224c3e329
Aide Sarahi <aideflores5874@alumnos.udg.mx>
2020-10-29 14:13:22 -0600

    actividad 13 terminada

--- main.cpp
+++ main.cpp
@@ -4,19 +4,31 @@ using namespace std;
 
 int main()
 {
-    Arreglo<float> arreglo;
-
-    for(size_t i = 0; i < 10; i++){
-        arreglo.insertar_final(i);
-    }
-    arreglo.insertar(100,2);
-    arreglo.eliminar_inicio();
-    arreglo.eliminar_final();
-    arreglo.eliminar(4);
+    Arreglo<string> arreglo;
+    //insertar 4 elementos
+    arreglo.insertar_final("hola1");
+    arreglo.insertar_final("hola2");
+    arreglo.insertar_inicio("hola3");
+    arreglo.insertar_inicio("hola4");
     for(size_t i = 0; i < arreglo.size(); i++){
         cout<<arreglo[i]<< " ";
     }
+    cout<<endl;
     
+    //insertar en la posicion 2
+    arreglo.insertar("hola5",2);
+    for(size_t i = 0; i < arreglo.size(); i++){
+        cout<<arreglo[i]<< " ";
+    }
+    cout<<endl;
+
+    //eliminar inicio, final y posicion 1
+    arreglo.eliminar_inicio();
+    arreglo.eliminar_final();
+    arreglo.eliminar(1);
+    for(size_t i = 0; i < arreglo.size(); i++){
+        cout<<arreglo[i]<< " ";
+    }
     cout<<endl;
 
     return 0;
